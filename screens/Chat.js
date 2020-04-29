a9ad7f1029492ffc3ed36a71931d07df42560fdb
Onur Uzun <onur.uzunn@hotmail.com>
2020-04-30 01:28:55 +0300

    chat entegrated

--- screens/Chat.js
+++ screens/Chat.js
@@ -4,14 +4,47 @@ import { GiftedChat } from 'react-native-gifted-chat';
 import { useNavigation, useRoute } from '@react-navigation/native';
 
 export default class Chat extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { messages: [] };
+        this.onSend = this.onSend.bind(this);
+    }
+    componentWillMount() {
+        this.setState({
+            messages: [
+                {
+                    _id: 1,
+                    text: 'Hello 🦄',
+                    createdAt: new Date(Date.UTC(2016, 7, 30, 17, 20, 0)),
+                    user: {
+                        _id: 2,
+                        name: 'React Native',
+                        avatar: 'https://facebook.github.io/react/img/logo_og.png',
+                    },
+                },
+            ],
+        });
+    }
+
+    onSend(messages = []) {
+        this.setState((previousState) => {
+            return {
+                messages: GiftedChat.append(previousState.messages, messages),
+            };
+        });
+    }
+
     render() {
         const { route, navigation } = this.props;
         return (
-            <View style={styles.container}>
-                <Text>{route.params?.first_name}</Text>
-
-                <Button title="Geri" onPress={() => navigation.navigate('Inbox')} />
-            </View>
+            <GiftedChat
+                messages={this.state.messages}
+                onSend={this.onSend}
+                user={{
+                    _id: 1,
+                }}
+            />
         )
     }
 }
